c6f9cee9fd41798761d2749d056aae1633c2e941
Chris Rudnicky <chrisrudnicky@gmail.com>
2019-03-30 13:22:33 -0700

    Add template data for adding a new to do task

---
+++ src/components/ToDoForm.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h4>Add a task to your to do list</h4>
+    <form>
+      <label for='task'>Task</label>
+      <div>
+        <input id='task' type='text' v-model='task' required autofocus>
+      </div>
+      <label for='importance'>Importance</label>
+      <div>
+        <select id='importance' v-model='importance' required>
+          <option value= 1>Urgent</option>
+          <option value= 2>Somewhat</option>
+          <option value= 3>Not at all</option>
+        </select>
+      </div>
+      <div>
+        <button type='submit' @click.prevent='newToDo'>Add to the list!</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  props
+}
